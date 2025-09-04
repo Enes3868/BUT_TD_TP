fc93505746a6324804789307b25de0221b621b02
e2400501 <enesyucel38@icloud.com>
2025-09-04 08:50:31 +0200

    n

---
+++ TD1-exo1.py
@@ -0,0 +1,9 @@
+def nbr(a: float, b: float) -> float:
+    """
+    Cette fonction retourne le plus grand des deux nombres réels.
+    """
+    return a if a > b else b
+
+print(nbr(2.7, 2.8))
+print(nbr(7.2, 7.2))
+
