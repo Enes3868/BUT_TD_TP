d28dacb45258fe040f4b7053cde187d4d9a3e0f6
e2400501 <enesyucel38@icloud.com>
2025-09-04 10:00:37 +0200

    n

--- TD1-exo2.py
+++ TD1-exo2.py
@@ -1,11 +1,42 @@
 class Tasse:
     attributmatiere : str = "ceramique"
-    def __init__(self, couleur: str, contenance:int, marque:str, valeur : int):
+    # Méthode init
+    def __init__(self, couleur: str, contenance:int, marque:str):
         self.couleur = couleur
         self.contenance = contenance
         self.marque = marque
-        self.attributmatiere = valeur
-
+    # Méthode str
     def __str__(self):
-        return f'la tasse de matière {self.attributmatiere}, de couleur {self.couleur} et de marque {self.marque} a une contenance de {self.contenance} ml'
+        return f'la tasse de matière {Tasse.attributmatiere}, de couleur {self.couleur} et de marque {self.marque} a une contenance de {self.contenance} ml'
 
+    # Méthode pour définir le contenu
+    def ajouter_contenu(self, contenu: str):
+        self.contenu = contenu
+
+    # Méthode pour supprimer le contenu
+    def boire(self):
+        if hasattr(self, 'contenu'):
+            del self.contenu
+
+
+tasse1 = Tasse("bleue", 50, "Duralex")
+
+"""
+Affichage de la tasse
+
+"""
+
+print(tasse1)
+
+"""
+la tasse de matière céramique, de couleur bleue et de marque Duralex a une contenance de 50 ml
+"""
+
+""" 
+Ajouter un contenu
+
+"""
+tasse1 = Tasse("bleue", 50, "Duralex")
+tasse1.ajouter_contenu("thé")
+
+print(f"{tasse1} Elle contient du {tasse1.contenu}.")
